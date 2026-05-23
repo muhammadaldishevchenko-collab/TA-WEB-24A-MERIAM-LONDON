c3ea7c302de2b8c4695e7b25d46be1c10b2d775a
muhammadaldishevchenko <muhammad_aldi_shevchenke@teknokrat.ac.id>
2026-05-23 10:33:16 +0700

    aldi:memperbaiki error pada layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,9 +29,16 @@ export default function RootLayout({
       lang="id"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <Navbar />
-      <body className="min-h-full flex flex-col">{children}</body>
-      <Footer />
+     
+      <body className="min-h-full flex flex-col">
+        <Navbar/>
+
+        <main className="flex-1">
+           {children}
+        </main>
+        <Footer/>
+      </body>
+      
     </html>
   );
 }
