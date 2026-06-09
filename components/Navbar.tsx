72250c2a6e18697277d3f59baa0979fdaee8ed83
muhammadaldishevchenko <muhammad_aldi_shevchenke@teknokrat.ac.id>
2026-06-09 21:15:44 +0700

    aldi:Menghapus navbar halaman kuis, tas darurat, dan ai-assistant

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -12,9 +12,6 @@ const navLinks = [
   { href: "/mitigasi", label: "Panduan Mitigasi" },
   { href: "/darurat", label: "Info Darurat" },
   { href: "/berita", label: "Berita" },
-  { href: "/kuis", label: "Quiz" },
-  { href: "/ai-assistant", label: "AI Assistant" },
-  { href: "/tas-darurat", label: "Tas darurat" },
 ];
 
 export default function Navbar() {
